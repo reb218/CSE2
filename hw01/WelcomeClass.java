bf48aefbcc08107c092bf12c02d0afb9cc8bbe41
reb218 <reb218@lehigh.edu>
2014-08-31 18:25:42 +0000

    created WelcomeClass program

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//Ryan Baitch
+//August 31 2014
+//WelcomeClass java program
+//
+//  First compile the program
+//    javac WelcomeClass.java
+//  Run the program
+//      java WelcomeClass
+//
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+  public static void main(String[] args) {
+
+//print the statement with box around it
+  System.out.println("  -----------");
+  System.out.println("  | WELCOME |");
+  System.out.println("  -----------");
+  System.out.println("  ^  ^  ^  ^  ^  ^");
+  System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+  System.out.println("|-R--E--B--2--1--8-|");
+  System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+  System.out.println("   v   v   v   v   v   v");
+  System.out.println("This past summer I worked at a sleepaway camp as a lifeguard and a counselor.");
+  }
+}
