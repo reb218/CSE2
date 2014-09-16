34ac1f1bce5d850312d64e8bcb8fb3337646638f
reb218 <reb218@lehigh.edu>
2014-09-16 23:52:02 +0000

    Root Program

--- hw03/Root.java
+++ hw03/Root.java
@@ -3,19 +3,25 @@
 //hw03-Root Program
 //September 13, 2014
 
-import java.util.Scanner;//import the Scanner
+import java.util.Scanner;
+//Import the Scanner
 
 //define the class
 public class Root{
 //add main method
     public static void main(String[]args){
-        Scanner myScanner;
-        myScanner=new Scanner(System.in);//Scanner allows program to accept input from user
+        Scanner myScanner; //Creates a new scanner for the program
+        myScanner=new Scanner(System.in); //Scanner allows program to accept input from user
         System.out.print(
-            "Enter any number as "+"a double (in the form of xx.xx): ");
-            double x=myScanner.nextDouble();
+             "Enter any double and I guess its cube root: ");
+            double x=myScanner.nextDouble(); //the variable x is what the user inputs
             double guess=x/3;
+            guess=((guess*guess*guess+x)/(3*guess*guess));
+            guess=((2*guess*guess*guess+x)/(3*guess*guess));
+            guess=((2*guess*guess*guess+x)/(3*guess*guess));
+            guess=((2*guess*guess*guess+x)/(3*guess*guess));
+            guess=((2*guess*guess*guess+x)/(3*guess*guess));
             
-        
+            System.out.println("The cube root is "+guess);
     }
 }
